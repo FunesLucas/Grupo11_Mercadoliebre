62ec9697efe2536f506260795839d7eec2f4bb3a
FunesLucas <funeslucas@outlook.com>
2021-08-19 21:49:03 -0300

    Agregando register and login.html

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ app.use(express.static(path.join(__dirname, "./public")));
 
 
 
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/index.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/login.html"));
 });
 
 
@@ -15,5 +22,5 @@ const port = process.env.PORT || '5000';
 
 app.listen(port,()=>{
 
-console.log('Server is runnig in the Port : ${port}');
+console.log(`Server is runnig in the Port : ${port}`);
 })
